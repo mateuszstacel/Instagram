58492a5a3eb66f3d2f6cd1b52842ef309295220a
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-12 10:53:24 +0000

    created post model, photo controller, Css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_134347) do
+ActiveRecord::Schema.define(version: 2019_01_12_103316) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "link"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
